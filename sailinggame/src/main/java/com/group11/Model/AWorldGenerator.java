83bb29dabbbbf7c181bdce812e05685bb0c191e5
William Norland <williamnorland@gmail.com>
2023-11-07 21:16:28 +0100

    Implementing AWorldGenerator

---
+++ sailinggame/src/main/java/com/group11/Model/AWorldGenerator.java
@@ -0,0 +1,13 @@
+package com.group11.Model;
+
+/**
+ * AWorldGenerator is an abstract baseclass for all types of 
+ * WorldGenerators, it represents the abstract idea of
+ * what a MapGenerator is/does (interface).
+ */
+public abstract class AWorldGenerator {
+    
+
+    // public abstract World generateWorld();
+
+}
