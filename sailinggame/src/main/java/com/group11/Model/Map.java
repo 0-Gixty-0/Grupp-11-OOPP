4290c2d9dee5e261fee76a7438e7e030d01b6759
William Norland <williamnorland@gmail.com>
2023-11-08 22:25:44 +0100

    World and Map objects

--- sailinggame/src/main/java/com/group11/Model/Map.java
+++ sailinggame/src/main/java/com/group11/Model/Map.java
@@ -1,39 +1,49 @@
 package com.group11.Model;
 
+import java.util.ArrayList;
+
 /**
  * Map is a representation of a two dimensional physical map in the form of an object containing Matrixes.
  */
 public class Map {
     
-    //private Tile [][] tileMatrix;
-    private int [][] graphicMatrix;
+    private Integer area;
+    private ArrayList<ArrayList<ATile>> tileMatrix;
+    private ArrayList<ArrayList<Integer>> graphicMatrix;
 
     /**
-     * Creates a Map object out of a TileMatrix and a GraphicsMatrix.
+     * Creates a Map object out of a TileMatrix and a GraphicsMatrix, Constructor is package procted because
+     * Maps should be created by map generator.
      * @param graphicsMatrix a matrix of ints each representing a certain tile texture.
+     * @param side the size of a side in a map, a map is always a square.
      */
-    public Map(/*Tile [][] tileMatrix*/ int [][] graphicMatrix ) {
+    protected Map(ArrayList<ArrayList<ATile>> tileMatrix, ArrayList<ArrayList<Integer>> graphicMatrix, Integer side) {
         this.graphicMatrix = graphicMatrix;
+        this.area = side*side;
     }
 
     /**
      * A getter for the graphicMatris attribute of the Map object.
-     * @return (int [][]) matrix of integers representing a certain tile texture.
+     * @return (ArrayList<ArrayList<Integer>>) matrix of integers representing a certain tile texture.
      */
-    public int [][] getGraphicMatrix() {
+    public ArrayList<ArrayList<Integer>> getGraphicMatrix() {
         return graphicMatrix;
     }
 
-    
+    /**
+     * A getter for the area attribute of the Map object.
+     * @return (Integer) area of the map
+     */
+    public Integer getArea() {
+        return area;
+    }
+
     /**
      *  A getter for the tileMatrix attribute of the Map object.
-     *  @return (Tile [][]) matrix of Tiles representing the map in the game logic.
+     *  @return (ArrayList<ArrayList<Tile>>) matrix of Tiles representing the map in the game logic.
      */
-     /*
-     * public Tile [][] getTileMatrix() {
-     *  return tileMatrix;
-     * }
-     */
-    
+    public ArrayList<ArrayList<ATile>> getTileMatrix() {
+    return tileMatrix;
+    }
 
 }
