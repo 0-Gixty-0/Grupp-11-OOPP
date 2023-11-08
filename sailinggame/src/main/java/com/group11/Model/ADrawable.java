bacfa64d2e2e817d6f0237c2e1a93d3483da0c30
Adam Kvarnsund <adam.kvarnsund17@gmail.com>
2023-11-08 14:39:49 +0100

    created ADrawable and ATile

---
+++ sailinggame/src/main/java/com/group11/Model/ADrawable.java
@@ -0,0 +1,9 @@
+package com.group11.Model;
+
+public abstract class ADrawable {
+    private int TEXTURE;
+
+    public int getTextureId(){
+        return TEXTURE;
+    }
+}
---
+++ sailinggame/src/main/java/com/group11/Model/ATile.java
@@ -0,0 +1,6 @@
+package com.group11.Model;
+
+public abstract class ATile extends ADrawable{
+    private Position pos;
+    private bool
+}
