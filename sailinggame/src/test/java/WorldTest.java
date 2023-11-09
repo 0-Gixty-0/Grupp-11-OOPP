3b70495c2df7759aa8e265237a67d2abeb874d1a
William Norland <williamnorland@gmail.com>
2023-11-09 14:27:55 +0100

    Implemented tests for task classes

--- sailinggame/src/test/java/WorldTest.java
+++ sailinggame/src/test/java/WorldTest.java
@@ -1,4 +1,29 @@
+import org.junit.Test;
+
+import com.group11.Model.AMapGenerator;
+import com.group11.Model.BasicMapGenerator;
+import com.group11.Model.BasicWorldGenerator;
+import com.group11.Model.Map;
+import com.group11.Model.World;
+
+import static org.junit.Assert.*;
+
 public class WorldTest {
     
+    @Test
+    public void testGetMap() {
+        AMapGenerator basicMapGenerator = new BasicMapGenerator();
+        World world = (new BasicWorldGenerator(basicMapGenerator)).generateWorld(100);
+        assertSame(Map.class, world.getMap().getClass());
+    }
 
+    @Test
+    public void testSetMap() {
+        AMapGenerator basicMapGenerator = new BasicMapGenerator();
+        World world = (new BasicWorldGenerator(basicMapGenerator)).generateWorld(100);
+        Map oldMap = world.getMap();
+        Map newMap = basicMapGenerator.generateMap(100);
+        world.setMap(newMap);
+        assertNotEquals(oldMap, world.getMap());
+    }
 }
