ccee4d3d86f4d8ad1d5ced4771e2e8d0ecf69e8d
William Norland <williamnorland@gmail.com>
2023-11-09 14:27:00 +0100

    Minor change to AMapGenerator

--- sailinggame/src/main/java/com/group11/Model/AMapGenerator.java
+++ sailinggame/src/main/java/com/group11/Model/AMapGenerator.java
@@ -1,7 +1,5 @@
 package com.group11.Model;
 
-import java.io.NotActiveException;
-
 /**
  * AMapGenerator is an abstract baseclass for all types of
  * MapGenerators, it represents the abstract idea of
@@ -14,15 +12,6 @@ public abstract class AMapGenerator {
      * @param side size of the map sides (map is square)
      * @return (Map) 
      */
-    public abstract Map generateMap(Integer side);
+    public abstract Map generateMap(int side);
 
-    /**
-     * Method for generating a a land (random which one) or sea tile randomly. 
-     * @param landBias a paramater for how big the probability of land should be, sea is therefore P(1-landBias).
-     * @return (Atile) returns a random tile
-     * @throws NotActiveException
-     */
-    public ATile randomTile(double landBias) throws NotActiveException {
-        throw new NotActiveException("Not implemented");
-    }
 }
