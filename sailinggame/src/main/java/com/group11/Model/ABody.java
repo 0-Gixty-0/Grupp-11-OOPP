2c581fc48ee506ce0ae5d595ba48001f041ffd41
cavassi <noa2009@live.se>
2023-11-11 00:53:11 +0100

    finished test classes, but module not fully completed

--- sailinggame/src/main/java/com/group11/Model/ABody.java
+++ sailinggame/src/main/java/com/group11/Model/ABody.java
@@ -3,6 +3,11 @@ package com.group11.Model;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * Abstract class representing an abstract body. Implements the IDamageable interface
+ * since a body can take damage.
+ */
+
 public abstract class ABody implements IDamageable {
 
     private ArrayList<Integer> dimension;
@@ -10,6 +15,13 @@ public abstract class ABody implements IDamageable {
     private int hitPoints;
     private int velocity;
 
+    /**
+     * Constructor for creating objects of type ABody.
+     * @param dimension - the dimension of the body
+     * @param pos       - the position of the body in the tilemap
+     * @param hitPoints - the hitpoints of the body
+     * @param velocity  - the velocity of the body
+     */
     public ABody(ArrayList<Integer> dimension, Point pos, int hitPoints, int velocity){
         this.dimension = dimension;
         this.pos = pos;
@@ -17,32 +29,67 @@ public abstract class ABody implements IDamageable {
         this.velocity = velocity;
     }
 
+    /**
+     * Reduces the hitpoints of the body
+     *
+     * @param damage - the amount of damage taken by the body
+     */
     @Override
     public void takeDamage(int damage){
         this.hitPoints -= damage;
     }
 
+    /**
+     * Returns the current hitpoints of the body
+     *
+     * @return the current hitpoints of the body
+     */
     public int getHitPoints() {
         return this.hitPoints;
     }
 
-    public void setHitPoints(int new_hitpoints) {
-        this.hitPoints = new_hitpoints;
+    /**
+     * Sets the hitpoints of the body
+     *
+     * @param newHitpoints - the new hitpoints value of the body
+     */
+    public void setHitPoints(int newHitpoints) {
+        this.hitPoints = newHitpoints;
     }
 
-    public void setVelocity(int new_velocity) {
-        this.velocity = new_velocity;
-    }
-
+    /**
+     * Returns the current velocity of the body
+     *
+     * @return the current velocity of the body
+     */
     public int getVelocity() {
         return this.velocity;
     }
 
+    /**
+     * Sets the velocity of the body
+     *
+     * @param newVelocity - the new velocity of the body
+     */
+    public void setVelocity(int newVelocity) {
+        this.velocity = newVelocity;
+    }
+
+    /**
+     * Returns the current position of the body
+     *
+     * @return the current position of the body
+     */
     public Point getPos(){
         return this.pos;
     }
 
-    public void setPos(Point new_position){
-        this.pos = new_position;
+    /**
+     * Sets the position of the body
+     *
+     * @param newPosition - the new position of the body
+     */
+    public void setPos(Point newPosition){
+        this.pos = newPosition;
     }
 }
--- sailinggame/src/main/java/com/group11/Model/IMovable.java
+++ sailinggame/src/main/java/com/group11/Model/IMovable.java
@@ -1,5 +1,8 @@
 package com.group11.Model;
 
+/**
+ * Interface IMovable represents the methods used for objects that are able to move.
+ */
 public interface IMovable {
     void move(int x, int y);
 }
