8a0f3470b5e492448ce47da2dd1855630497191d
William <williamnorland@gmail.com>
2023-11-11 20:15:23 +0100

    Added attr and constructor to AMovableEntity

--- sailinggame/src/main/java/com/group11/Model/AMovableEntity.java
+++ sailinggame/src/main/java/com/group11/Model/AMovableEntity.java
@@ -1,5 +1,12 @@
 package com.group11.model;
 
-public abstract class AMovableEntity {
-    //AMovableBody body;
+public abstract class AMovableEntity extends AEntity implements ICommandable {
+
+    private AMovableBody body;
+
+    public AMovableEntity(AMovableBody body, String name, Boolean friendly) {
+        super(name, friendly);
+        this.body = body;
+    }
+    
 }
