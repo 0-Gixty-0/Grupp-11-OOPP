98f2c26f0d5c70f47a8ca0d9a7fd397c526d6b16
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-11-11 20:20:51 +0100

    Added Exception to AbstractViewTile and created tests

--- sailinggame/src/main/java/com/group11/view/AbstractViewTile.java
+++ sailinggame/src/main/java/com/group11/view/AbstractViewTile.java
@@ -18,12 +18,15 @@ public abstract class AbstractViewTile implements Drawable {
 
     /**
      * Constructor for creating objects of type tile.
-     * @param id - terrain type number
+     * @param id - terrain type number, must be between 0 - 15
      * @param dimension - width and height of tile
      * @param matrixPosition - tile position in matrix (row, column)
      * @param pixelPosition - tile position on window frame in x and y pixels
      */
     public AbstractViewTile(int id, Dimension dimension, Point matrixPosition, Point pixelPosition) {
+        if (id < 0 || id > 15) {
+            throw new IllegalArgumentException("Invalid terrain ID");
+        }
         this.id = id;
         this.dimension = dimension;
         this.matrixPosition = matrixPosition;
@@ -79,9 +82,10 @@ public abstract class AbstractViewTile implements Drawable {
     }
 
     /**
-     * Gets the coordinates for a tile image in the texture map based on a tile id number
-     * @param id The id for terrain type
-     * @return The point coordinate in the texture map matrix
+     * Gets the coordinates for a tile image in the texture map based on a tile id number.
+     * Coordinates are (row, column) with values 0 - 3
+     * @param id The id for terrain type.
+     * @return The point coordinate in the texture map matrix (row, column)
      */
     private Point getTextureMatrixCoordinate(int id) {
         return new Point((int) Math.floor(id/4), id % 4);
