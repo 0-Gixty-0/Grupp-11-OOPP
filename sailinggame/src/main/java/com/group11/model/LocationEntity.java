30dcf683df7504036f8722c3c8fad07a6669da7d
William <williamnorland@gmail.com>
2023-11-13 22:32:30 +0100

    Implemented LocationEntity

--- sailinggame/src/main/java/com/group11/model/LocationEntity.java
+++ sailinggame/src/main/java/com/group11/model/LocationEntity.java
@@ -1,18 +1,34 @@
 package com.group11.model;
 
+/**
+ * A class representing the subset of fixed, immovable, entities in the game world. For example port cities.
+ */
 public class LocationEntity extends AEntity {
 
     private ABody body;
 
-    public LocationEntity(String name, Boolean friendly) {
+    /**
+     * Constructor used to create LocationEntities.
+     * @param body The body of the location.
+     * @param name The name of the location.
+     * @param friendly If the location is of friendly allegience to the player.
+     */
+    public LocationEntity(ABody body, String name, Boolean friendly) {
         super(name, friendly);
-        //TODO Auto-generated constructor stub
     }
 
+    /**
+     * 
+     * @return (ABody) of the LocationEntity
+     */
     public ABody getBody() {
         return body;
     }
 
+    /**
+     * 
+     * @param body New body for the LocationEntity
+     */
     public void setBody(ABody body) {
         this.body = body;
     }
