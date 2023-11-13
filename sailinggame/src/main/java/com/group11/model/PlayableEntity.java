4405a02ea7df56fe33edf1c046f9efb67e1bc14c
William <williamnorland@gmail.com>
2023-11-13 21:45:43 +0100

    Implemented PlayableEntity class

--- sailinggame/src/main/java/com/group11/model/PlayableEntity.java
+++ sailinggame/src/main/java/com/group11/model/PlayableEntity.java
@@ -3,52 +3,52 @@ import java.awt.Point;
 
 public class PlayableEntity extends AMovableEntity {
 
-    public PlayableEntity(AMovableBody body, String name) {
-        super(body, name, true);
+  public PlayableEntity(AMovableBody body, String name) {
+      super(body, name, true);
 
-    }
+  }
 
-    /**
-     * A Helper method for movecommand implementation in PlayableEntity using pseudo linear algebra.
-     * @param velocity The amount of tiles each move moves.
-     * @param dirVector The direction the body should move in.
-     */
-    private void moveHelper(int velocity, int [] dirVector) {
-      for (int i = 0; i < velocity; i++ ) {
-          Point currPos = this.getBody().getPos();
-          int currX = (int) currPos.getX();
-          int currY = (int) currPos.getY();
-          this.getBody().move(currX + dirVector[0],currY + dirVector[1]);
-      }
+  /**
+   * A Helper method for movecommand implementation in PlayableEntity using pseudo linear algebra.
+   * @param velocity The amount of tiles each move moves.
+   * @param dirVector The direction the body should move in.
+   */
+  private void moveHelper(int velocity, int [] dirVector) {
+    for (int i = 0; i < velocity; i++ ) {
+        Point currPos = this.getBody().getPos();
+        int currX = (int) currPos.getX();
+        int currY = (int) currPos.getY();
+        this.getBody().move(currX + dirVector[0],currY + dirVector[1]);
     }
+  }
 
-    /**
-     * Issue a move command to a entity in 8 difference directions using these integers as arguments:
-     * 0 moves straight up,
-     * 1 moves at a right angle up,
-     * 2 moves straight right,
-     * 3 moves at a right angle down,
-     * 4 moves at straight down,
-     * 5 moves at a left angle down,
-     * 6 moves straight left,
-     * 7 move at a left angle up,
-     * @param direction Moves the Entity in the chosen direction (see the index above).
-     */
-    @Override
-    public void moveCommand(Integer direction) {
-        int velocity = this.getBody().getVelocity();
-        int[][] directions = {{0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}};
-        this.moveHelper(velocity, directions[direction]);
-    }
+  /**
+   * Issue a move command to a entity in 8 difference directions using these integers as arguments:
+   * 0 moves straight up,
+   * 1 moves at a right angle up,
+   * 2 moves straight right,
+   * 3 moves at a right angle down,
+   * 4 moves at straight down,
+   * 5 moves at a left angle down,
+   * 6 moves straight left,
+   * 7 move at a left angle up,
+   * @param direction Moves the Entity in the chosen direction (see the index above).
+   */
+  @Override
+  public void moveCommand(Integer direction) {
+      int velocity = this.getBody().getVelocity();
+      int[][] directions = {{0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}};
+      this.moveHelper(velocity, directions[direction]);
+  }
 
-    @Override
-    public void attackCommand(Integer direction) {
-        //Placeholder for later implementation.
-    }
+  @Override
+  public void attackCommand(Integer direction) {
+      //Placeholder for later implementation.
+  }
 
-    @Override
-    public void interactCommand() {
-        //Placeholder for later implementation.
-    }
+  @Override
+  public void interactCommand() {
+      //Placeholder for later implementation.
+  }
   
 }
