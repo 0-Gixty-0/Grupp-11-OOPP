bb1aece089bd202ee97805d69157c579ad2c4b9e
William <williamnorland@gmail.com>
2023-11-13 22:33:03 +0100

    Made tests for PlayableEntity

---
+++ sailinggame/src/test/java/PlayableEntityTest.java
@@ -0,0 +1,105 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.group11.model.PlayableEntity;
+import com.group11.model.Ship;
+
+public class PlayableEntityTest {
+    
+    @Test
+    public void testMoveUp() {
+        Ship testShip = new Ship(1,10,0, true);
+        PlayableEntity player = new PlayableEntity(testShip, "testy mcTest");
+        player.getBody().setVelocity(2);
+        player.moveCommand(0);
+        int playerY = (int) player.getBody().getPos().getY();
+        int playerX = (int) player.getBody().getPos().getX();
+        assertEquals(2, playerY);
+        assertEquals(0, playerX);
+    }
+
+    @Test
+    public void testMoveUpRightAngle() {
+        Ship testShip = new Ship(1,10,0, true);
+        PlayableEntity player = new PlayableEntity(testShip, "testy mcTest");
+        player.getBody().setVelocity(2);
+        player.moveCommand(1);
+        int playerY = (int) player.getBody().getPos().getY();
+        int playerX = (int) player.getBody().getPos().getX();
+        assertEquals(2, playerY);
+        assertEquals(2, playerX);
+    }
+
+    @Test
+    public void testMoveRight() {
+        Ship testShip = new Ship(1,10,0, true);
+        PlayableEntity player = new PlayableEntity(testShip, "testy mcTest");
+        player.getBody().setVelocity(2);
+        player.moveCommand(2);
+        int playerY = (int) player.getBody().getPos().getY();
+        int playerX = (int) player.getBody().getPos().getX();
+        assertEquals(0, playerY);
+        assertEquals(2, playerX);
+    }
+
+    @Test
+    public void testMoveDownRightAngle() {
+        Ship testShip = new Ship(1,10,0, true);
+        PlayableEntity player = new PlayableEntity(testShip, "testy mcTest");
+        player.getBody().setVelocity(2);
+        player.moveCommand(3);
+        int playerY = (int) player.getBody().getPos().getY();
+        int playerX = (int) player.getBody().getPos().getX();
+        assertEquals(-2, playerY);
+        assertEquals(2, playerX);
+    }
+
+    @Test
+    public void testMoveDown() {
+        Ship testShip = new Ship(1,10,0, true);
+        PlayableEntity player = new PlayableEntity(testShip, "testy mcTest");
+        player.getBody().setVelocity(2);
+        player.moveCommand(4);
+        int playerY = (int) player.getBody().getPos().getY();
+        int playerX = (int) player.getBody().getPos().getX();
+        assertEquals(-2, playerY);
+        assertEquals(0, playerX);
+    }
+
+    @Test
+    public void testMoveDownLeftAngle() {
+        Ship testShip = new Ship(1,10,0, true);
+        PlayableEntity player = new PlayableEntity(testShip, "testy mcTest");
+        player.getBody().setVelocity(2);
+        player.moveCommand(5);
+        int playerY = (int) player.getBody().getPos().getY();
+        int playerX = (int) player.getBody().getPos().getX();
+        assertEquals(-2, playerY);
+        assertEquals(-2, playerX);
+    }
+
+    @Test
+    public void testMoveLeft() {
+        Ship testShip = new Ship(1,10,0, true);
+        PlayableEntity player = new PlayableEntity(testShip, "testy mcTest");
+        player.getBody().setVelocity(2);
+        player.moveCommand(6);
+        int playerY = (int) player.getBody().getPos().getY();
+        int playerX = (int) player.getBody().getPos().getX();
+        assertEquals(0, playerY);
+        assertEquals(-2, playerX);
+    }
+
+    @Test
+    public void testMoveUpLeftAngle() {
+        Ship testShip = new Ship(1,10,0, true);
+        PlayableEntity player = new PlayableEntity(testShip, "testy mcTest");
+        player.getBody().setVelocity(2);
+        player.moveCommand(7);
+        int playerY = (int) player.getBody().getPos().getY();
+        int playerX = (int) player.getBody().getPos().getX();
+        assertEquals(2, playerY);
+        assertEquals(-2, playerX);
+    }
+}
+
