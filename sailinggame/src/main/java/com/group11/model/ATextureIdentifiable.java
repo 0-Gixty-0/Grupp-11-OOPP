6ade36302a8146594cd3b6d74b0b36463db2bd92
William Norland <williamnorland@gmail.com>
2023-11-14 16:59:04 +0100

    Had to modify ATextureIdentifiable to proceed with task

--- sailinggame/src/main/java/com/group11/model/ATextureIdentifiable.java
+++ sailinggame/src/main/java/com/group11/model/ATextureIdentifiable.java
@@ -3,6 +3,10 @@ package com.group11.model;
 public abstract class ATextureIdentifiable {
     private int texture;
 
+    protected ATextureIdentifiable(int textureId) {
+        this.texture = textureId;
+    }
+
     public int getTextureId(){
         return this.texture;
     }
