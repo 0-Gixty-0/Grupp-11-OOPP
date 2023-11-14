c53d8fe77165917f25a5fa5fc11b7e2bb2c11f53
William Norland <williamnorland@gmail.com>
2023-11-14 13:41:41 +0100

    Making tests for LocationEntity

--- sailinggame/src/test/java/LocationEntityTest.java
+++ sailinggame/src/test/java/LocationEntityTest.java
@@ -1,3 +1,27 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.group11.model.ABody;
+import com.group11.model.LocationEntity;
+import com.group11.model.Ship;
+
 public class LocationEntityTest {
     
+    @Test
+    public void testGetBody() {
+        ABody ship = new Ship(null, null, 0, 0, 0, 0);
+        LocationEntity location = new LocationEntity(ship, "ShipTown", false);
+        assertEquals(ship, location.getBody());
+    }
+
+    @Test
+    public void testSetBody() {
+        ABody ship = new Ship(null, null, 0, 0, 0, 0);
+        LocationEntity location = new LocationEntity(null, "ShipTown", false);
+        assertEquals(null, location.getBody());
+        location.setBody(ship);
+        assertEquals(ship, location.getBody());
+    }
 }
+  
