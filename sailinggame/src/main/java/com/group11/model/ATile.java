c5f27625455ec1f1f69456b23d406a60c9cf682a
Adam Kvarnsund <adam.kvarnsund17@gmail.com>
2023-11-15 15:10:50 +0100

    Added changes to the TAS-23 branch

--- sailinggame/src/main/java/com/group11/model/ATile.java
+++ sailinggame/src/main/java/com/group11/model/ATile.java
@@ -1,20 +1,44 @@
 package com.group11.model;
 
 import java.awt.Point;
-
+/**
+ * This class is used as an abstraction for the tile object
+ */
 public abstract class ATile extends ATextureIdentifiable{
-    
+
+    /**
+     * The position of a tile as a Java Point.
+     */
     private Point pos;
+
+    /**
+     * Boolean that checks if the tile is passable.
+     */
     private Boolean passable;
 
+    /**
+    * Constructor for the abstract class ATile
+    * @param textureId - The texture ID for this specific tile
+    * @param pos - The position of a tile as a Java Point 
+    * @param passable - Boolean that checks if the tile is passable
+    */
     protected ATile(int textureId, Point pos, Boolean passable ) {
         super(textureId);
+        this.pos = pos;
+        this.passable = passable;
     }
-    
+
+    /**
+    * Returns the position of a tile
+    * @return A Java Point for the position of the tile
+    */
     public Point getPos(){
         return this.pos;
     }
-
+    /**
+    * Returns if the tile is passable or not
+    * @return A boolean that says if the current point is passable or not
+    */
     public Boolean isPassable(){
         return this.passable;
     }
--- sailinggame/src/main/java/com/group11/model/SeaTile.java
+++ sailinggame/src/main/java/com/group11/model/SeaTile.java
@@ -1,9 +1,15 @@
 package com.group11.model;
 
 import java.awt.Point;
-
+/**
+* This class represents a concrete implementation for sea tiles, extending the abstract class ATile.
+*/
 public class SeaTile extends ATile {
 
+    /**
+    * Constructor for the class SeaTile
+    * @param pos - The position of a tile as a Java Point 
+    */
     protected SeaTile(Point pos) {
         super(0, pos, true);
     }
