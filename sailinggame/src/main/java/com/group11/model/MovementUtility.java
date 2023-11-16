09a4d18962fffe36a212b2fd5e19deb810468401
William Norland <williamnorland@gmail.com>
2023-11-16 16:18:49 +0100

    Maintaing old and adding new utility classes

---
+++ sailinggame/src/main/java/com/group11/model/MovementUtility.java
@@ -0,0 +1,31 @@
+package com.group11.model;
+
+import java.awt.Point;
+
+public final class MovementUtility {
+
+    private static Map map;
+
+    private MovementUtility() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void setMap(Map map) {
+        MovementUtility.map = map;
+    }
+
+    public static Boolean movementIsPossible(Point currPos, int[] dirVector) {
+        if (MovementUtility.map == null) {
+            throw new IllegalStateException("Unable to move because a Map has not been set for MovementUtility");
+        }
+
+        int newX = (int) currPos.getX() + dirVector[0];
+        int newY = (int) currPos.getY() + dirVector[1];
+
+        if (MovementUtility.map.getTileMatrix().get(newY).get(newX).isPassable()) {
+            return true;
+        }
+
+        return false;
+    }
+}
