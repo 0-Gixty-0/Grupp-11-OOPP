59094a9f4822dcd97dae3d23b5ac0e4024fef1c5
William Norland <williamnorland@gmail.com>
2023-11-16 16:19:01 +0100

    Modyfing and adding tests

--- sailinggame/src/test/java/TileMatrixDecoderTest.java
+++ sailinggame/src/test/java/TileMatrixDecoderTest.java
@@ -12,10 +12,10 @@ import com.group11.model.TileMatrixDecoder;
 public class TileMatrixDecoderTest {
 
     @Test
-    public void testDecode() {
+    public void testDecodeIntoIntMatrix() {
         Map map = (new BasicMapGenerator()).generateMap(50);
         ArrayList<ArrayList<ATile>> tileMatrix = map.getTileMatrix();
-        ArrayList<ArrayList<Integer>> gMap = TileMatrixDecoder.decode(tileMatrix);
+        ArrayList<ArrayList<Integer>> gMap = TileMatrixDecoder.decodeIntoIntMatrix(tileMatrix);
         
         assertEquals(0, (int) gMap.get(0).get(0));
         assertEquals(1, (int) gMap.get(25).get(25));
