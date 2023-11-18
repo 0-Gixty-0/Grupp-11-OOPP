aa61a6248204398dfc568df228c27ef361b40765
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-11-18 11:06:30 +0100

    Add JDOC for factory interface and implementation

--- sailinggame/src/main/java/com/group11/model/IMovableEntityFactory.java
+++ sailinggame/src/main/java/com/group11/model/IMovableEntityFactory.java
@@ -1,8 +1,19 @@
 package com.group11.model;
 
+/**
+ * Interface for a factory creating movable entities
+ */
 public interface IMovableEntityFactory {
+    /**
+     * Signature for creating a player
+     * @return
+     */
     AEntity createPlayer();
 
+    /**
+     * Signature for creating an enemy
+     * @return
+     */
     AEntity createEnemy();
 
 }
--- sailinggame/src/main/java/com/group11/model/StandardMovableEntityFactory.java
+++ sailinggame/src/main/java/com/group11/model/StandardMovableEntityFactory.java
@@ -3,18 +3,36 @@ package com.group11.model;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * Class representing a factory for creating standard movable entities
+ */
 public class StandardMovableEntityFactory implements IMovableEntityFactory {
+    /**
+     * Creates a movable entity of type AEntity representing a standard player.
+     * The method creates a body of type Ship for the entity and a starting position for the ship.
+     * @return Object of type AEntity including a body and position
+     */
     public AEntity createPlayer() {
         ArrayList<ArrayList<Boolean>> dimensions = createDimensions(1);
         return new PlayableEntity(new Ship(dimensions, new Point(0,0), 0, 0 ,0 ,0, 0), "player");
     }
 
+    /**
+     * Creates a movable entity of type AEntity representing a standard enemy.
+     * The method creates a body of type Ship for the entity and a starting position for the ship.
+     * @return Object of type AEntity including a body and position
+     */
     public AEntity createEnemy() {
         // TODO
         // Create an unplayable entity class
         return null;
     }
 
+    /**
+     * Method creating the dimensions parameter for a Ship from a set size where matrix is (size x size)
+     * @param size Size of square matrix
+     * @return Square matrix of size (size) containing all true values
+     */
     private ArrayList<ArrayList<Boolean>> createDimensions(int size) {
         ArrayList<ArrayList<Boolean>> dimensions = new ArrayList<>();
         for (int i = 1; i <= size; i++) {
