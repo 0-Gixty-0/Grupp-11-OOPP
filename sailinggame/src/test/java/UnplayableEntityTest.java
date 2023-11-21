8f5d89718f32caaa9e3e3f336bcc61dfe6a56722
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-11-21 13:40:57 +0100

    Create UnplayableEntityTest

---
+++ sailinggame/src/test/java/UnplayableEntityTest.java
@@ -0,0 +1,6 @@
+import org.junit.Test;
+
+public class UnplayableEntityTest {
+    // Currently no new methods or functions to test that are contained solely in UnplayableEntity
+    // Constructor has only a super call so the abstract class it inherits from should be tested for the constructor
+}
