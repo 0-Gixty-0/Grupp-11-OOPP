3f142c7e16c3c5e25944b90b0193994b3d87abd5
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-11-21 13:36:47 +0100

    Create UnplayableEntity class and implement moveCommand

---
+++ sailinggame/src/main/java/com/group11/model/UnplayableEntity.java
@@ -0,0 +1,31 @@
+package com.group11.model;
+
+public class UnplayableEntity extends AMovableEntity {
+
+    /**
+     * Used to create a movable entity
+     *
+     * @param body     The body it should use
+     * @param name     The entities name
+     * @param friendly A single factor which assigns the life long allegience of the entity
+     */
+    protected UnplayableEntity(AMovableBody body, String name, Boolean friendly) {
+        super(body, name, friendly);
+    }
+
+    @Override
+    public void moveCommand(Integer direction) {
+        int[][] directions = {{0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}};
+        this.moveHelper(directions[direction]);
+    }
+
+    @Override
+    public void attackCommand(Integer direction) {
+        // Currently not implemented
+    }
+
+    @Override
+    public void interactCommand() {
+        // Currently not implemented
+    }
+}
