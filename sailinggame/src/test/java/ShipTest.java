0b1e6524cf5f5711df0471f89607d63869b8fe28
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-11-22 17:28:08 +0100

    Add test for default constructor

--- sailinggame/src/test/java/ShipTest.java
+++ sailinggame/src/test/java/ShipTest.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 
 import com.group11.model.Ship;
 
+import java.awt.*;
+
 import static org.junit.Assert.*;
 
 public class ShipTest {
@@ -23,5 +25,15 @@ public class ShipTest {
         assertEquals(testShip.getSailStatus(), true);
         testShip.lowerSail();
         assertEquals(testShip.getSailStatus(),false);
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        Ship ship = new Ship(new Point(0,0));
+        assertEquals(1, ship.getShipLevel());
+        assertEquals(2, ship.getArmor());
+        assertEquals(5, ship.getCannons());
+        assertTrue(ship.getSailStatus());
+        assertFalse(ship.getAnchorStatus());
     }
 }
