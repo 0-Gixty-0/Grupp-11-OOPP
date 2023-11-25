b0af5f278ee695a7fd6ba912927865709feb63cd
William Norland <williamnorland@gmail.com>
2023-11-25 18:47:02 +0100

    Adding prototype

--- sailinggame/src/main/java/com/group11/Main.java
+++ sailinggame/src/main/java/com/group11/Main.java
@@ -1,35 +1,49 @@
 package com.group11;
 
-import com.group11.controller.GlobalKeyListener;
-import com.group11.model.*;
-import com.group11.view.AppWindow;
+import java.awt.Point;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class Main {
+import com.group11.controller.GlobalKeyListener;
+import com.group11.model.AMovableBody;
+import com.group11.model.BasicMapGenerator;
+import com.group11.model.BasicWorldGenerator;
+import com.group11.model.CommandableEntity;
+import com.group11.model.IMapGenerator;
+import com.group11.model.IWorldGenerator;
+import com.group11.model.MovementUtility;
+import com.group11.model.Ship;
+import com.group11.model.TileMatrixDecoder;
+import com.group11.model.World;
+import com.group11.view.AppWindow;
+import com.group11.view.ViewTileMatrixEncoder;
+
+class Main {
     private AppWindow appWindow;
     private static final GlobalKeyListener keyboardController = new GlobalKeyListener();
     private static int windowHeight;
     private static int windowWidth;
     private World world;
     private CommandableEntity player;
+    private ArrayList<ArrayList<Integer>> playerMatrix;
 
     public Main(int windowWidth, int windowHeight) {
+
         windowHeight = windowHeight;
         windowWidth = windowWidth;
-        this.appWindow = new AppWindow(windowWidth, windowHeight);
+        this.appWindow = new AppWindow(windowHeight, windowHeight, 50, 50, 16, 16);
         this.world = this.createBasicWorld();
         this.player = this.createBasicPlayer();
         MovementUtility.setTileMatrix(this.world.getMap().getTileMatrix());
+        this.appWindow.updateTerrain(ViewTileMatrixEncoder.createTerrainTileMatrix(TileMatrixDecoder.decodeIntoIntMatrix(world.getMap().getTileMatrix())));
     }
 
     private World createBasicWorld() {
         IMapGenerator mapGenerator = new BasicMapGenerator();
         IWorldGenerator worldGenerator = new BasicWorldGenerator(mapGenerator);
-        return worldGenerator.generateWorld(47);
+        return worldGenerator.generateWorld(50);
     }
 
     private CommandableEntity createBasicPlayer() {
@@ -37,20 +51,74 @@ public class Main {
         return new CommandableEntity(ship, "Player", true);
     }
 
+    private List<List<Integer>> generatePlayerMatrix(int newPosX, int newPosY) {
+        List<List<Integer>> playerMatrix = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < 50; j++) {
+                if (i == newPosX && j == newPosY) {
+                    row.add(0);
+                } else {
+                    row.add(-1);
+                }
+            }
+            playerMatrix.add(row);
+        }
+        return playerMatrix;
+    }
+
     private void decodeController() {
         Set<Integer> request = Main.keyboardController.getInput();
-        ArrayList<ArrayList<Integer>> validInputs = new ArrayList<>();
-        ArrayList<Integer> input = new ArrayList<>();
-        input.add(87);
-        validInputs.add(input);
 
-        for (ArrayList<Integer> valInput : validInputs) {
-            if (request.containsAll(valInput)) {
-                this.player.moveCommand(0);
-                System.out.println("Moved up");
-                System.out.println(this.player.getPos());
-            }
+        if (request.contains(65) && request.contains(83)) {    
+            this.player.moveCommand(1);
+            int newPosX = (int) this.player.getPos().getX();
+            int newPosY = (int) this.player.getPos().getY();
+            appWindow.updateEntities(ViewTileMatrixEncoder.createEntityTileMatrix(generatePlayerMatrix( newPosX, newPosY)));
         }
+        if (request.contains(65) && request.contains(87)) {
+            this.player.moveCommand(7);
+            int newPosX = (int) this.player.getPos().getX();
+            int newPosY = (int) this.player.getPos().getY();
+            appWindow.updateEntities(ViewTileMatrixEncoder.createEntityTileMatrix(generatePlayerMatrix( newPosX, newPosY)));
+        }
+        if (request.contains(68) && request.contains(83)) {
+            this.player.moveCommand(3);
+            int newPosX = (int) this.player.getPos().getX();
+            int newPosY = (int) this.player.getPos().getY();
+            appWindow.updateEntities(ViewTileMatrixEncoder.createEntityTileMatrix(generatePlayerMatrix( newPosX, newPosY)));
+        }
+        if (request.contains(65) && request.contains(87)) {
+            this.player.moveCommand(5);
+            int newPosX = (int) this.player.getPos().getX();
+            int newPosY = (int) this.player.getPos().getY();
+            appWindow.updateEntities(ViewTileMatrixEncoder.createEntityTileMatrix(generatePlayerMatrix( newPosX, newPosY)));
+        }
+        if (request.contains(87)) {
+            this.player.moveCommand(6);
+            int newPosX = (int) this.player.getPos().getX();
+            int newPosY = (int) this.player.getPos().getY();
+            appWindow.updateEntities(ViewTileMatrixEncoder.createEntityTileMatrix(generatePlayerMatrix( newPosX, newPosY)));
+        }
+        if (request.contains(68)) {
+            this.player.moveCommand(4);
+            int newPosX = (int) this.player.getPos().getX();
+            int newPosY = (int) this.player.getPos().getY();
+            appWindow.updateEntities(ViewTileMatrixEncoder.createEntityTileMatrix(generatePlayerMatrix( newPosX, newPosY)));
+        }
+        if (request.contains(65)) {
+            this.player.moveCommand(0);
+            int newPosX = (int) this.player.getPos().getX();
+            int newPosY = (int) this.player.getPos().getY();
+            appWindow.updateEntities(ViewTileMatrixEncoder.createEntityTileMatrix(generatePlayerMatrix( newPosX, newPosY)));
+        }
+        if (request.contains(83)) {
+            this.player.moveCommand(2);
+            int newPosX = (int) this.player.getPos().getX();
+            int newPosY = (int) this.player.getPos().getY();
+            appWindow.updateEntities(ViewTileMatrixEncoder.createEntityTileMatrix(generatePlayerMatrix( newPosX, newPosY)));
+        }
+        
     }
 
     /**
@@ -60,7 +128,7 @@ public class Main {
         appWindow.showGame();
         while (true) {
             this.decodeController();
-            Thread.sleep(100);
+            Thread.sleep(50);
         }
     }
 
