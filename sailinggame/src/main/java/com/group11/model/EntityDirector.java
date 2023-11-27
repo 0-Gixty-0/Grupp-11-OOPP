cec5983e13fc500361f687427a020310c2665088
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-11-27 18:08:11 +0100

    Create createPlayer and createEnemy methods in EntityDirector

--- sailinggame/src/main/java/com/group11/model/EntityDirector.java
+++ sailinggame/src/main/java/com/group11/model/EntityDirector.java
@@ -1,5 +1,7 @@
 package com.group11.model;
 
+import java.awt.*;
+
 public class EntityDirector {
     private EntityBuilder builder;
 
@@ -11,13 +13,24 @@ public class EntityDirector {
         this.builder = builder;
     }
 
-    public AEntity createPlayer() {
+    public AEntity createPlayer(Point position) {
         builder.reset();
-
+        builder.setName("Player");
+        builder.setFriendlyStatus(true);
+        builder.setPosition(position);
+        builder.setAttributesForLevel(1);
+        builder.setBody();
+        return builder.createEntity();
     }
 
-    public AEntity createEnemy(int lvl) {
-
+    public AEntity createEnemy(Point position, int lvl) {
+        builder.reset();
+        builder.setName(String.format("Enemy: lvl %d",lvl));
+        builder.setFriendlyStatus(true);
+        builder.setPosition(position);
+        builder.setAttributesForLevel(lvl);
+        builder.setBody();
+        return builder.createEntity();
     }
 }
 
