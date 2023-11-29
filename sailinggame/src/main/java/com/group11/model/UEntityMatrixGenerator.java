326cdf5ca455b866819eeb478cbfac002a0220e4
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-11-29 15:44:41 +0100

    Add JDOC

--- sailinggame/src/main/java/com/group11/model/UEntityMatrixGenerator.java
+++ sailinggame/src/main/java/com/group11/model/UEntityMatrixGenerator.java
@@ -2,9 +2,21 @@ package com.group11.model;
 
 import java.util.ArrayList;
 
+/**
+ * UEntityMatrixGenerator has static methods for creating an empty entity matrix and populating with entity objects
+ */
 public final class UEntityMatrixGenerator {
+    /**
+     * Utility class does not need to be instantiated
+     */
     private UEntityMatrixGenerator() {System.out.println("Utility class cannot be instantiated");}
 
+    /**
+     * Creates an entity matrix with all values set to null
+     * @param width Number of columns in matrix
+     * @param height Number of rows in matrix
+     * @return Entity matrix, all values null
+     */
     public static ArrayList<ArrayList<AEntity>> createEntityMatrix(int width, int height) {
         ArrayList<ArrayList<AEntity>> entityMatrix = new ArrayList<>();
         for (int rowIndex = 0; rowIndex < height; rowIndex++) {
@@ -17,6 +29,12 @@ public final class UEntityMatrixGenerator {
         return entityMatrix;
     }
 
+    /**
+     * Adds entities from list to an entity matrix in correct position according to body of entity
+     * @param entities List of entities to add to entity matrix
+     * @param entityMatrix Entity matrix to add entities to
+     * @return Entity matrix with input entities placed in correct position
+     */
     public static ArrayList<ArrayList<AEntity>> populateEntityMatrix(ArrayList<AEntity> entities, ArrayList<ArrayList<AEntity>> entityMatrix) {
         for (AEntity entity : entities) {
             int rowPosition = entity.getBody().getPos().y;
