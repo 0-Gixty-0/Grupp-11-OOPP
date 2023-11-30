3f7c5d734080e1f8f7502534f5ce9da55c2c7291
Duchessmuhr <alle.muhr@hotmail.com>
2023-11-30 15:21:24 +0100

    broke out input to dir and rewrote tests

--- sailinggame/src/main/java/com/group11/controller/AControllerInterpretor.java
+++ sailinggame/src/main/java/com/group11/controller/AControllerInterpretor.java
@@ -1,15 +1,51 @@
 package com.group11.controller;
 
+import java.util.Set;
+
 public abstract class AControllerInterpretor{
 
-    private int dir;
+    /*
+     * Function that converts the Keylistener code of keyboard input to a direction 0-7 clockwise starting upwards
+     */
+        
 
-    public int inputToDir(){
-        return dir;
-    }
-    
-    public int shootDir(){
-        return dir;
+    public static int inputToDir(Set<Integer> inputSet, int up, int down, int left, int right) {
+
+        /*Direction up-left */
+        if (inputSet.contains(up) && inputSet.contains(right) && !inputSet.contains(left) && !inputSet.contains(down)) {
+            return 1;
+        }
+         /*Direction down-left */
+         if (inputSet.contains(down) && inputSet.contains(right) && !inputSet.contains(up) && !inputSet.contains(left)){
+            return 3;
+        }
+        /*Direction down-right */
+        if (inputSet.contains(left) && inputSet.contains(down) && !inputSet.contains(right) && !inputSet.contains(up)) {
+            return 5;
+        }
+        /*Direction up-left */
+        if (inputSet.contains(up) && inputSet.contains(left) && !inputSet.contains(right) && !inputSet.contains(down)) {
+            return 7;
+        }
+        /* Direction up*/
+        if (inputSet.contains(up) && !inputSet.contains(down)) {
+            return 0;
+        }
+        /*Direction right */
+        if (inputSet.contains(right) && !inputSet.contains(left)) {
+            return 2;
+        }
+        /*Direction down */
+        if (inputSet.contains(down) && !inputSet.contains(up)) {
+            return 4;
+        }
+        /*Direction left */
+        if (inputSet.contains(left) && !inputSet.contains(right)) {
+            return 6;
+        }
+
+        /*Returns -1 if none of the inputs corresponds to a direction (-1) is not a valid direction */
+        return -1;
     }
 
 }
--- sailinggame/src/main/java/com/group11/controller/KeyboardInterpretor.java
+++ sailinggame/src/main/java/com/group11/controller/KeyboardInterpretor.java
@@ -9,83 +9,17 @@ public class KeyboardInterpretor extends AControllerInterpretor{
     public KeyboardInterpretor(Set<Integer> inputSet){
         
     }        
-    /*
-     * Function that converts the ascii code of keyboard input to a direction 0-7 clockwise starting upwards
-     */
-        public static int inputToDir(Set<Integer> inputSet) {
 
-            /*Direction up-left */
-            if (inputSet.contains((int) 'w') && inputSet.contains((int) 'd') && !inputSet.contains((int) 'a') && !inputSet.contains((int) 's')) {
-                return 1;
-            }
-             /*Direction down-left */
-             if (inputSet.contains((int) 's') && inputSet.contains((int) 'd') && !inputSet.contains((int) 'w') && !inputSet.contains((int) 'a')) {
-                return 3;
-            }
-            /*Direction down-right */
-            if (inputSet.contains((int) 'a') && inputSet.contains((int) 's') && !inputSet.contains((int) 'd') && !inputSet.contains((int) 'w')) {
-                return 5;
-            }
-            /*Direction up-left */
-            if (inputSet.contains((int) 'w') && inputSet.contains((int) 'a') && !inputSet.contains((int) 'd') && !inputSet.contains((int) 's')) {
-                return 7;
-            }
-            /* Direction up*/
-            if (inputSet.contains((int) 'w') && !inputSet.contains((int) 's')) {
-                return 0;
-            }
-            /*Direction right */
-            if (inputSet.contains((int) 'd') && !inputSet.contains((int) 'a')) {
-                return 2;
-            }
-            /*Direction down */
-            if (inputSet.contains((int) 's') && !inputSet.contains((int) 'w')) {
-                return 4;
-            }
-            /*Direction left */
-            if (inputSet.contains((int) 'a') && !inputSet.contains((int) 'd')) {
-                return 6;
-            }
+        public static int arrowsToDir(Set<Integer> inputSet){
 
-            /*Returns -1 if none of the inputs corresponds to a direction (-1) is not a valid direction */
-            return -1;
+            return inputToDir(inputSet, 38, 40, 37,39);
         }
-    
-        public static int shootDir(Set<Integer> inputSet){
-            /*Direction up-left */
-            if (inputSet.contains(38) && inputSet.contains(39) && !inputSet.contains(37) && !inputSet.contains(40)) {
-                return 1;
-            }
-             /*Direction down-left */
-             if (inputSet.contains(40) && inputSet.contains(39) && !inputSet.contains(38) && !inputSet.contains(37)) {
-                return 3;
-            }
-            /*Direction down-right */
-            if (inputSet.contains(37) && inputSet.contains(40) && !inputSet.contains(39) && !inputSet.contains(38)) {
-                return 5;
-            }
-            /*Direction up-left */
-            if (inputSet.contains(37) && inputSet.contains(37) && !inputSet.contains(39) && !inputSet.contains(40)) {
-                return 7;
-            }
-            /* Direction up*/
-            if (inputSet.contains(37) && !inputSet.contains(40)) {
-                return 0;
-            }
-            /*Direction right */
-            if (inputSet.contains(39) && !inputSet.contains(37)) {
-                return 2;
-            }
-            /*Direction down */
-            if (inputSet.contains(40) && !inputSet.contains(38)) {
-                return 4;
-            }
-            /*Direction left */
-            if (inputSet.contains(37) && !inputSet.contains(39)) {
-                return 6;
-            }
-            return -1;
+        
+        public static int WASDToDir(Set<Integer> inputSet){
+
+            return inputToDir(inputSet, 87, 83, 65, 68);
         }
+        
     }
 
     
