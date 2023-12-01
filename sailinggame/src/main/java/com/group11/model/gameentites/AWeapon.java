d0bbae965d7404cb88559b13902f80ff74556209
cavassi <35598467+cavassi@users.noreply.github.com>
2023-12-01 16:54:50 +0100

    Javadoc finished, only README left

--- sailinggame/src/main/java/com/group11/model/gameentites/AWeapon.java
+++ sailinggame/src/main/java/com/group11/model/gameentites/AWeapon.java
@@ -3,13 +3,29 @@ package com.group11.model.gameentites;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This abstract class represents a weapon in the game. A weapon is capable of firing projectiles.
+ *
+ * A weapon has a list of projectiles that it has fired and a maximum number of times it can be fired.
+ */
 public abstract class AWeapon {
 
-    List<AProjectile> firedProjectiles;
-    int maxTimesFired;
+    protected List<AProjectile> firedProjectiles;
+    protected int maxTimesFired;
+
+    /**
+     * Constructs a new weapon.
+     *
+     * Initializes the list of fired projectiles.
+     */
     protected AWeapon() {
         this.firedProjectiles = new ArrayList<AProjectile>();
     }
 
+    /**
+     * Fires the weapon.
+     *
+     * This method must be implemented by subclasses.
+     */
     public abstract void fireWeapon();
 }
--- sailinggame/src/main/java/com/group11/model/gameentites/ZigZagCannonBall.java
+++ sailinggame/src/main/java/com/group11/model/gameentites/ZigZagCannonBall.java
@@ -1,10 +1,29 @@
 package com.group11.model.gameentites;
 
+/**
+ * This class represents a ZigZagCannonBall in the game. A ZigZagCannonBall is a type of projectile that can be fired by a weapon.
+ *
+ * A ZigZagCannonBall has a distance it has traveled, a maximum range it can travel, the damage it can cause,
+ * the direction it is traveling in, and the hitpoints it has. A ZigZagCannonBall always has 1 hitpoint.
+ *
+ * The ZigZagCannonBall has a unique movement pattern where it moves in a zigzag path.
+ */
 public class ZigZagCannonBall extends AProjectile {
+
+    /**
+     * Constructs a new ZigZagCannonBall with the given parameters.
+     *
+     * The distance traveled is initialized to 0, the maximum range is 50, the damage is 50, the direction is {0, 0}, and the hitpoints is 1.
+     */
     ZigZagCannonBall() {
-        super(0, 50, 50, new int[]{0, 0});
+        super(0, 50, 50, new int[]{0, 0}, 1);
     }
 
+    /**
+     * Moves the ZigZagCannonBall along its zigzag travel path if possible.
+     *
+     * The direction of the movement is determined by the current direction of the ZigZagCannonBall.
+     */
     @Override
     protected void moveInTravelPath() {
 
