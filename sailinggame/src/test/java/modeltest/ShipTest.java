54eccd5a17c5b090ce6d5c74a9e27bd628ce56e5
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-12-06 16:36:21 +0100

    Extend ShipTest with fireWeaponTest

--- sailinggame/src/test/java/modeltest/ShipTest.java
+++ sailinggame/src/test/java/modeltest/ShipTest.java
@@ -1,14 +1,47 @@
 package modeltest;
 
-import static org.junit.Assert.assertEquals;
-
+import com.group11.model.gameentites.*;
 import org.junit.Test;
 
-import com.group11.model.gameentites.Ship;
+import java.awt.*;
+
+import static org.junit.Assert.*;
 
 public class ShipTest {
 
-    Ship testShip = new Ship(null, 0, 0, null, 2);
+    private class TestShip extends Ship {
+        private TestWeapon weaponBasic;
+
+        public TestShip(Point pos, int shipLevel, double armor, AWeapon weapon, double hitPoints) {
+            super(pos, shipLevel, armor, weapon, hitPoints);
+            this.weaponBasic = (TestWeapon) weapon;
+        }
+
+        public TestWeapon getTestWeapon() {
+            return this.weaponBasic;
+        }
+    }
+
+    private class TestWeapon extends BasicCannon {
+
+        public TestWeapon(Class<? extends AProjectile> projectileType) {
+            super(projectileType);
+        }
+
+        public int debugFiredProjectilesSize() {
+            return this.getFiredProjectilesSize();
+        }
+
+        public Class<? extends AProjectile> debugGetProjectileType() {
+            return this.getProjectileType();
+        }
+        public AProjectile getProjectile() {
+            return this.getFiredProjectiles().get(0);
+        }
+
+    }
+
+    TestShip testShip = new TestShip(new Point(0,0), 0, 0, new TestWeapon(BasicCannonBall.class), 2);
 
     @Test
     public void testGetShipLevel() {
@@ -22,11 +55,16 @@ public class ShipTest {
 
     @Test
     public void testGetCannons() {
-        assertEquals(testShip.getWeapon(), null);
+        assertTrue(testShip.getWeapon() instanceof BasicCannon);
     }
 
     @Test
     public void testFireWeapon() {
-        // Method is not implemented yet.
+        testShip.fireWeapon(new int[] {0, 1});
+        Object projectile = testShip.getTestWeapon().debugGetProjectileType();
+        Point pos = new Point(0,1);
+        assertEquals(1, testShip.getTestWeapon().debugFiredProjectilesSize());
+        assertSame(projectile, BasicCannonBall.class);
+        assertTrue(pos.equals(testShip.getTestWeapon().getProjectile().getPos()));
     }
 }
