0f2e2629114febba7db6ba99a62a1ed63a4f320a
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2023-12-06 13:58:36 +0100

    Create method for firing entity weapons in AICommander

--- sailinggame/src/main/java/com/group11/model/utility/AICommander.java
+++ sailinggame/src/main/java/com/group11/model/utility/AICommander.java
@@ -17,6 +17,7 @@ public class AICommander {
     private final int radius = 10;
     private List<List<AEntity>> entityMatrix;
     private List<List<Integer>> terrainMatrixEncoded;
+    private final int[][] directions = {{-1,0}, {-1,1}, {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}};
 
     public AICommander(List<List<AEntity>> entityMatrix, List<List<ATile>> terrainMatrix) {
         this.entityMatrix = entityMatrix;
@@ -51,7 +52,7 @@ public class AICommander {
         for (CommandableEntity enemy : enemies) {
             int entityRowIndex = enemy.getBody().getPos().x;
             int entityColumnIndex = enemy.getBody().getPos().y;
-            HashMap<String, Point> namePosMap = this.getSurroundingEntityNameAndPos(entityRowIndex, entityColumnIndex);
+            HashMap<String, Point> namePosMap = this.getSurroundingEntityNameAndPos(entityRowIndex, entityColumnIndex, 10);
             if (namePosMap.containsKey("Player")) {
                 Point playerPoint = namePosMap.get("Player");
                 Point enemyPoint = enemy.getPos();
@@ -66,14 +67,12 @@ public class AICommander {
     /**
      * Helper method checking if the input entity is in proximity to the player
      * @param entity The entity to center the proximity search
-     * @param entityMatrix The entity matrix to search in
      * @return True: Player is near, False: Player is not near
      */
-    private boolean isNearPlayer(CommandableEntity entity, List<List<AEntity>> entityMatrix) {
+    private boolean isNearPlayer(CommandableEntity entity, int radius) {
         int entityRowIndex = entity.getBody().getPos().y;
         int entityColumnIndex = entity.getBody().getPos().x;
-        int radius = 5;
-        HashMap<String, Point> surroundingEntities = this.getSurroundingEntityNameAndPos(entityRowIndex, entityColumnIndex);
+        HashMap<String, Point> surroundingEntities = this.getSurroundingEntityNameAndPos(entityRowIndex, entityColumnIndex, radius);
         if (surroundingEntities.containsKey("Player")) {
             return true;
         } else {
@@ -88,14 +87,14 @@ public class AICommander {
      * @param col Column index of center
      * @return HashMap with keys as entity names and values as entity positions
      */
-    private HashMap<String, Point> getSurroundingEntityNameAndPos(int row, int col) {
+    private HashMap<String, Point> getSurroundingEntityNameAndPos(int row, int col, int radius) {
         HashMap<String, Point> surroundingElements = new HashMap<>();
 
         int n = this.entityMatrix.size();
 
         // Check bounds and add surrounding elements within the given radius
-        for (int i = row - this.radius; i <= row + this.radius; i++) {
-            for (int j = col - this.radius; j <= col + this.radius; j++) {
+        for (int i = row - radius; i <= row + radius; i++) {
+            for (int j = col - radius; j <= col + radius; j++) {
                 if (i >= 0 && i < n && j >= 0 && j < n && !(i == row && j == col)) {
                     if (this.entityMatrix.get(i).get(j) != null) {
                         String name = this.entityMatrix.get(i).get(j).getName();
@@ -106,5 +105,35 @@ public class AICommander {
             }
         }
         return surroundingElements;
+    }
+
+    private boolean isPathClear(int[] direction, Point start, Point goal) {
+        while (start != goal) {
+            Point newPos = new Point(start.x + direction[0], start.y + direction[1]);
+            if (this.terrainMatrixEncoded.get(newPos.x).get(newPos.y) == 0) {
+                return false;
+            } else {
+                start.setLocation(newPos);
+            }
+        }
+        return true;
+    }
+
+    public void fireWeapons(List<CommandableEntity> entities) {
+        for (CommandableEntity entity : entities) {
+            int entityRowIndex = entity.getBody().getPos().x;
+            int entityColumnIndex = entity.getBody().getPos().y;
+            HashMap<String, Point> namePosMap = this.getSurroundingEntityNameAndPos(entityRowIndex, entityColumnIndex, 15);
+            if (namePosMap.containsKey("Player")) {
+                Point playerPoint = namePosMap.get("Player");
+                Point enemyPoint = entity.getPos();
+                int dx = playerPoint.x - enemyPoint.x;
+                int dy = playerPoint.y - enemyPoint.y;
+                int fireDirection = AStar.getDirection(dx,dy);
+                if (this.isPathClear(this.directions[fireDirection], enemyPoint, playerPoint)) {
+                    entity.attackIfAble(fireDirection);
+                }
+            }
+        }
     }
 }
