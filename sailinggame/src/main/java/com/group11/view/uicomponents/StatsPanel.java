0aa5215822df928cb4c84d27e55d9b13c1612de3
cavassi <noa2009@live.se>
2023-12-07 16:48:17 +0100

    Added javadoc

--- sailinggame/src/main/java/com/group11/view/uicomponents/StatsPanel.java
+++ sailinggame/src/main/java/com/group11/view/uicomponents/StatsPanel.java
@@ -6,15 +6,46 @@ import javax.swing.*;
 
 /**
  * The class is a JPanel that is used as a buffer panel containing the stats of the player.
+ * It contains labels for the player's level, hit points, and score, as well as icons for movement and shooting.
+ * The layout of the panel is a centered FlowLayout.
  */
+
 public class StatsPanel extends BufferPanel {
-    
+
+    /**
+     * JLabel for displaying the player's score.
+     */
     private JLabel scoreLabel;
+
+
+    /**
+     * JLabel for displaying the player's level.
+     */
     private JLabel lvlLabel;
+
+    /**
+     * JLabel for displaying the player's hit points.
+     */
     private JLabel hpLabel;
+
+    /**
+     * JLabel for displaying the movement icon.
+     */
     private JLabel movementIconLabel;
+
+    /**
+     * JLabel for displaying the shooting icon.
+     */
     private JLabel arrowsIconLabel;
 
+    /**
+     * Constructs a new StatsPanel with a specified width and height.
+     * Initializes the score, level, and hitpoints labels, as well as the movement and shooting icons.
+     * Sets the layout to a centered FlowLayout.
+     *
+     * @param width the width of the panel
+     * @param height the height of the panel
+     */
     protected StatsPanel(int width, int height) {
         super(width, height);
         lvlLabel = new JLabel("LVL: 0");
@@ -23,6 +54,7 @@ public class StatsPanel extends BufferPanel {
 
         Icon arrowsIcon = new ImageIcon(getClass().getResource("/shoot.png"));
         arrowsIconLabel = new JLabel(arrowsIcon);
+
         Icon movementIcon = new ImageIcon(getClass().getResource("/move.png"));
         movementIconLabel = new JLabel(movementIcon);
 
@@ -39,16 +71,30 @@ public class StatsPanel extends BufferPanel {
         this.add(hpLabel);
     }
 
+    /**
+     * Sets the text of the score label to the specified score.
+     *
+     * @param score the player's score
+     */
     protected void setScoreLabel(int score) {
         this.scoreLabel.setText("SCORE: " + score);
     }
 
+    /**
+     * Sets the text of the level label to the specified level.
+     *
+     * @param lvl the player's level
+     */
     protected void setLvlLabel(int lvl) {
         this.lvlLabel.setText("LVL: " + lvl);
     }
 
+    /**
+     * Sets the text of the hit points label to the specified hit points.
+     *
+     * @param hp the player's hit points
+     */
     protected void setHpLabel(int hp) {
         this.hpLabel.setText("HP: " + hp);
     }
-
 }
