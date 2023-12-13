1356245ca77cb960499a5f6d8eacaa2a4e281fc7
William <williamnorland@gmail.com>
2023-12-13 14:49:26 +0100

    Implemented a PlayerManager

---
+++ sailinggame/src/main/java/com/group11/model/modelinitialization/PlayerManager.java
@@ -0,0 +1,36 @@
+package com.group11.model.modelinitialization;
+
+import com.group11.model.gameentites.CommandableEntity;
+
+/**
+ * Class for managing the player
+ */
+public class PlayerManager {
+
+    private CommandableEntity player;
+
+    public PlayerManager(CommandableEntity player) {
+        this.player = player;
+    }
+
+    /**
+     * Gets the score of the player
+     * @return the score of the player
+     */
+    protected int getPlayerScore() {
+        return ScoreBoard.getScore(player);
+    }
+
+    /**
+     * Updates player position based on keyboard input
+     */
+    protected void updatePlayer(int movementInput, int fireInput) {
+
+        if (movementInput >= 0) {
+            this.player.moveIfPossible(movementInput);
+        }
+        if (fireInput >= 0) {
+            this.player.attackIfPossible(fireInput);
+        }
+    }
+}
