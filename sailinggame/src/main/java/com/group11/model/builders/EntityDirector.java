e5f6506ebe52c16c1b74fc74b8acd892674e887d
William <williamnorland@gmail.com>
2023-12-13 16:07:22 +0100

    Changing starting level for player

--- sailinggame/src/main/java/com/group11/model/builders/EntityDirector.java
+++ sailinggame/src/main/java/com/group11/model/builders/EntityDirector.java
@@ -33,7 +33,8 @@ public class EntityDirector {
         builder.setName("Player");
         builder.setFriendlyStatus(true);
         builder.setPosition(position);
-        builder.setAttributesForLevel(1);
+        builder.setAttributesForLevel(0);
+        builder.setAttributesForLevel(5);
         builder.setBody();
         return builder.createEntity();
     }
